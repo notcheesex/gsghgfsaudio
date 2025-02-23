6b1d7f4c1371bbb5d1dfde301943b552f29f0d88
notcheesex <73028156+notcheesex@users.noreply.github.com>
2025-02-23 15:57:07 +0300

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,6 +2,7 @@ local inputChest = peripheral.wrap("left")
 local badItemsChest = peripheral.wrap("top")
 local goodItemsChest = peripheral.wrap("right")
 badlist = {'relics:infinity_ham', 'minecraft:enchanted_book'}
+badlist2 = {'minecraft:enchanted_book'}
 
 function table_contains(tbl, x)
   found = false
@@ -18,8 +19,7 @@ while true do
 
     local itemDetails = inputChest.getItemDetail(slot)
     if not table_contains(badlist, itemDetails.name) then
-      print(itemDetails)
-      if itemDetails.nbt or itemDetails.damage then
+      if table_contains(badlist2, itemDetails.name) or if itemDetails.nbt or itemDetails.damage then
         inputChest.pushItems(peripheral.getName(badItemsChest), slot)
       else
         inputChest.pushItems(peripheral.getName(goodItemsChest), slot)
